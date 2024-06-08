750f68b17e96daf7656ac3ed879d3de705df01f6
p-wojciechowski <61585694+p-wojciechowski@users.noreply.github.com>
2024-06-08 12:20:07 +0200

    add train/val split for medi dataset

--- scripts/convert_medi_to_arrow.py
+++ scripts/convert_medi_to_arrow.py
@@ -1,33 +1,63 @@
+"""
+Converts raw medi dataset into Arrow format. It creates train/val split 80/20
+with stratification based on the task_name.
+
+Parameters:
+    MEDI_JSON (str): Path to the MEDI JSON file.
+    ARROW_FILENAME (str): Path to the Arrow file.
+    OVERWRITE (bool): If True, overwrite the existing Arrow file.
+
+You can load the Arrow dataset using the following code:
+
+```python
+from datasets import load_from_disk
+dataset = load_from_disk("data/medi/medi.arrow")
+
+train_dataset = dataset['train']
+val_dataset = dataset['val']
+```
+"""
+
+import os
+import shutil
 import json
-from datasets import Dataset
+from datasets import Dataset, DatasetDict
 import pandas as pd
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 MEDI_JSON = "data/medi/medi-data.json"
 ARROW_FILENAME = "data/medi/medi.arrow"
+OVERWRITE = False
 
-# Load your JSON data
-with open(MEDI_JSON, 'r') as f:
+if os.path.exists(ARROW_FILENAME):
+    if OVERWRITE:
+        shutil.rmtree(ARROW_FILENAME)
+    else:
+        raise FileExistsError(f"The directory '{ARROW_FILENAME}' already exists.")
+
+
+with open(MEDI_JSON, "r") as f:
     data = json.load(f)
 
-
-# Prepare the data in the format expected by Hugging Face
 processed_data = []
 for item in tqdm(data):
-    for i in range(len(item['query'])):
-        processed_data.append({
-            'query': item['query'][1],
-            'pos': item['pos'][1],
-            'neg': item['neg'][1],
-            'task_name': item['task_name']
-        })
+    for i in range(len(item["query"])):
+        processed_data.append(
+            {"query": item["query"][1], "pos": item["pos"][1], "neg": item["neg"][1], "task_name": item["task_name"]}
+        )
 
-# Convert to Pandas DataFrame
 df = pd.DataFrame(processed_data)
-
-# Convert to Hugging Face Dataset
 dataset = Dataset.from_pandas(df)
 
-# Save to Arrow format
-dataset.save_to_disk(ARROW_FILENAME)
+train_indices, val_indices = train_test_split(
+    range(len(dataset)), test_size=0.2, stratify=df["task_name"], random_state=42
+)
+
+train_dataset = dataset.select(train_indices)
+val_dataset = dataset.select(val_indices)
+
+dataset_dict = DatasetDict({"train": train_dataset, "val": val_dataset})
+
+dataset_dict.save_to_disk(ARROW_FILENAME)
 print(f"Saved to {ARROW_FILENAME}")
