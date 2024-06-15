c2adeb9e0f2fbe5d057cacb1e92bfd342475d20e
p-wojciechowski <61585694+p-wojciechowski@users.noreply.github.com>
2024-06-15 15:21:40 +0200

    [not ready] start parametrizing mted eval script

--- scripts/evaluate_on_mteb.py
+++ scripts/evaluate_on_mteb.py
@@ -1,34 +1,48 @@
+
+
 import mteb
 from sentence_transformers import SentenceTransformer
 from matryoshka_experiment.evaluation.model_mixins import PrependPromptMixin
 
+
+DIMENSIONS = [96, 192, 384, 768]
+MODELS_TYPES = ["ff", "mrl"]
 OUR_TASKS = [
     (
         "AmazonCounterfactualClassification",
-        "Represent the Amazon review for counterfactual classification; Input: ",
+        "Represent the Amazon review for counterfactual classification;",
     )
 ]
 
+def get_model(model_type: str, dim: int) -> SentenceTransformer:
+    if model_type == "ff":
+        return SentenceTransformer(f"mim-nlp-project/ff-{dim}", revision="v2-finetuning-checkpoint-015000")
+    elif model_type == "mrl":
+        return SentenceTransformer(f"mim-nlp-project/mrl", revision="v2-finetuning-checkpoint-015000",
+                                   truncate_dim=dim)
+    else:
+        raise ValueError(f"Invalid model type: {model_type}, it should be either 'ff' or 'mrl'")
+
+def export_results_to_dataframe():
+    pass
+
+
 
 def main():
-    repo_id = "mim-nlp-project/ff-768"
-    version = "v1"
-    checkpoint_step = 18000
+    for model_type, dimensionality in product(MODELS_TYPES, DIMENSIONS):
 
-    model_name_or_path = repo_id
-    revision = f"{version}-checkpoint-{str(checkpoint_step).zfill(6)}"
-    model = SentenceTransformer(model_name_or_path, revision=revision)
-    for task_name, prompt in OUR_TASKS:
-        model = PrependPromptMixin(prompt, model, debug=False)
-        tasks = mteb.get_tasks(tasks=[task_name])
-        evaluation = mteb.MTEB(tasks, task_langs=["en"])
-        results = evaluation.run(
-            model,
-            output_folder=f"evaluation_results/{model_name_or_path}-{revision}/{task_name}",
-        )
-        print(results)
+        model = get_model(model_type, dimensionality)
 
-    return results
+        for task_name, prompt in OUR_TASKS:
+            model = PrependPromptMixin(prompt, model, debug=False)
+            print(f"Model: {model_type}, Dimensionality: {dimensionality}, Task: {task_name}")
+            # tasks = mteb.get_tasks(tasks=[task_name])
+            # evaluation = mteb.MTEB(tasks, task_langs=["en"])
+            # results = evaluation.run(
+            #     model,
+            #     output_folder=f"evaluation_results/{model_type}_{dimensionality}/{task_name}",
+            # )
+            # print(results)
 
 
 if __name__ == "__main__":
