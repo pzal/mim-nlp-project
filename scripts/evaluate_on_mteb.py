86b5b7e07095a5518a0a21c53f3be2cc02a2651e
Pawel <pw431929@students.mimuw.edu.pl>
2024-06-15 16:38:11 +0200

    [not ready] parametrize mteb eval script, add prompts to go

--- scripts/evaluate_on_mteb.py
+++ scripts/evaluate_on_mteb.py
@@ -1,12 +1,32 @@
 from itertools import product
+import joblib
+from dotenv import load_dotenv
 
 import mteb
 from sentence_transformers import SentenceTransformer
 from matryoshka_experiment.evaluation.model_mixins import PrependPromptMixin
+import neptune
+
+load_dotenv()
+load_dotenv(".public_env")
 
 
 DIMENSIONS = [96, 192, 384, 768]
 MODELS_TYPES = ["ff", "mrl"]
+
+# OUR_TASKS=[  ("AmazonCounterfactualClassification",),
+#    ( "AmazonPolarityClassification",),
+#    ( "AmazonReviewsClassification",),
+#    ( "Banking77Classification",),
+#    ( "EmotionClassification",),
+#    ( "ImdbClassification",),
+#    ( "MassiveIntentClassification",),
+#    ( "MassiveScenarioClassification",),
+#    ( "MTOPDomainClassification",),
+#    ( "MTOPIntentClassification",),
+#    ( "ToxicConversationsClassification",),
+#     ("TweetSentimentExtractionClassification", )]
+
 OUR_TASKS = [
     (
         "AmazonCounterfactualClassification",
@@ -34,6 +54,12 @@ def main():
         model = get_model(model_type, dimensionality)
 
         for task_name, prompt in OUR_TASKS:
+            run = neptune.init_run(tags=["mteb_evaluation"])
+            run["model_type"] = model_type
+            run["dimensionality"] = dimensionality
+            run["mteb_task_name"] = task_name
+            run["prompt"] = prompt
+
             model = PrependPromptMixin(prompt, model, debug=False)
             print(f"Model: {model_type}, Dimensionality: {dimensionality}, Task: {task_name}")
             tasks = mteb.get_tasks(tasks=[task_name], languages=["eng"])
@@ -41,8 +67,17 @@ def main():
             results = evaluation.run(
                 model,
                 output_folder=f"evaluation_results/{model_type}_{dimensionality}/{task_name}",
+                verbosity=3,
+                overwrite_results=True
             )
-            # print(results)
+            for result in results[0].scores['test']:
+                if result['hf_subset'] == 'en':
+                    run["test/main_score"] = result['main_score']
+                    run["test/all_metrics"] = result
+                    break
+            joblib.dump(results, f"evaluation_results/{model_type}_{dimensionality}/{task_name}/results.joblib")
+            print(results)
+            run.stop()
 
 
 if __name__ == "__main__":
